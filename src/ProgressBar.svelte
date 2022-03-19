eba0124202dc81f390d80571eec8196df2cc3bbb
John Bell <jbellny@gmail.com>
2022-03-19 11:05:58 -0400

    committing html

---
+++ src/ProgressBar.svelte
@@ -0,0 +1,21 @@
+<div bp='grid'>
+    <div bp="offset-4@md 4@md 12@sm" class="progress-container">
+        <div 
+            class="progress-bar" 
+            style="width: 20%">
+            <span class="sr-only">%</span>
+        </div>
+    </div>
+</div>
+
+<style>
+    .progress-container {
+        margin: 20px 0;
+        border: solid rgba(194,194,194);
+    }
+
+    .progress-bar {
+        height: 30px;
+        background-color: rgb(77,199,55);
+    }
+</style>
